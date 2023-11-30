9b6c730bc25c1e5fef46d7c035914967d16146f3
xskide <xskide.code@gmail.com>
2023-12-01 01:42:56 +0300

    first commit

---
+++ constants/index.js
@@ -0,0 +1,123 @@
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  python,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+import surgeHero from "../public/surgeHero.png";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: python,
+    name: "Python",
+    type: "Software Development",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const projects = [
+    {
+      iconUrl: threads,
+      imageUrl: surgeHero,
+      theme: 'btn-back-pink',
+      name: 'Full Stack Youtube Clone',
+      description: 'Developed a web application youtube clone that focuses on small channels and advertising.',
+      link: 'https://www.connectnsurge.com/',
+  },    
+];
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,39 @@
 import { useCallback } from "react";
+import { skills, projects } from "../constants";
+import { arrow } from "../assets/icons";
+import Link from "next/link";
+import Image from "next/image";
 
 const PortfolioTemplate = () => {
-  const onButtonClick = useCallback(() => {
-    window.open("https://www.animaapp.com");
-  }, []);
-
   return (
     <div className="relative bg-midnightblue-300 w-full flex flex-col items-center justify-start">
-      <header className="self-stretch bg-midnightblue-600 flex flex-row flex-wrap items-center justify-between py-[0.63rem] pr-[8.75rem] pl-[2.63rem] sm:flex-row sm:gap-[1.5rem] sm:items-start sm:justify-start">
+      <header className="self-stretch bg-midnightblue-600 flex flex-row items-center justify-between py-[0.63rem] pr-[8.75rem] pl-[2.63rem] sm:pl-1 sm:gap-[1.8rem] sm:items-center ">
         <img
-          className="relative w-[5.13rem] h-[3.06rem] object-cover"
+          className="relative w-[5.13rem] h-[3.06rem] object-cover sm:w-[3.5rem] sm:h-[2.5rem]"
           alt=""
           src="/slice-1-1@2x.png"
         />
-        <nav className="m-0 flex flex-row items-start justify-start gap-[1.88rem] text-left text-[1.06rem] text-cornsilk-500 font-fredoka-one">
-          <a className="[text-decoration:none] relative leading-[1.69rem] text-[inherit] flex items-center w-[3.25rem] shrink-0">
+        <nav className="m-0 flex flex-row items-start justify-start gap-[1.88rem] sm:gap-2 text-left text-[1.06rem] text-cornsilk-500 font-fredoka-one">
+          <Link href="#about" className="[text-decoration:none] relative leading-[1.69rem] text-[inherit] flex items-center w-[3.25rem] shrink-0">
             About
-          </a>
-          <div className="relative leading-[1.69rem] text-cornsilk-700 flex items-center w-[4.19rem] shrink-0">
+          </Link>
+          <Link href="#projects" className="[text-decoration:none] relative leading-[1.69rem] text-cornsilk-700 flex items-center w-[4.19rem] shrink-0">
             Projects
-          </div>
-          <div className="relative leading-[1.69rem] text-cornsilk-100 flex items-center w-[4.13rem] shrink-0">
+          </Link>
+          <Link href="#contact" className="[text-decoration:none] relative leading-[1.69rem] text-cornsilk-100 flex items-center w-[4.13rem] shrink-0">
             Contact
-          </div>
+          </Link>
         </nav>
       </header>
-      <section className="self-stretch bg-midnightblue-100 overflow-hidden flex flex-row items-center justify-between py-[0.25rem] px-[0rem]">
+      <section className="self-stretch bg-midnightblue-100 overflow-hidden flex flex-row items-center justify-center py-[0.25rem] px-[0rem]">
         <img
-          className="relative w-[79.88rem] h-[35.63rem] object-cover"
+          className="relative w-[79.88rem] h-[35.63rem] sm:w-[59.88rem] sm:h-[25.63rem] object-cover"
           alt=""
           src="/slice-2-1@2x.png"
         />
       </section>
-      <section className="self-stretch bg-cornsilk-200 flex flex-row flex-wrap items-center justify-between py-[2.63rem] px-[1.5rem] text-left text-[1.25rem] text-black font-body-text sm:items-center sm:justify-center">
-        <div className="w-[38.75rem] flex flex-row items-center justify-center gap-[3rem] min-w-[20rem] max-w-[38.75rem] sm:flex-col">
+      <section className="self-stretch bg-cornsilk-200 flex flex-row flex-wrap items-center justify-center py-[2.63rem] px-[1.5rem] text-left text-[1.25rem] text-black font-body-text sm:items-center sm:justify-center">
+        <div className="w-[38.75rem] flex flex-row items-center justify-center gap-[3rem] min-w-[18rem] max-w-[38.75rem] sm:flex-col">
           <div className="flex-1 flex flex-col items-start justify-start gap-[0.94rem] sm:flex-[unset] sm:self-stretch">
             <h2 className="m-0 self-stretch relative text-inherit leading-[1.88rem] font-semibold font-inherit">{`Hi I’m Leon `}</h2>
             <h1 className="m-0 self-stretch relative text-[2.25rem] font-semibold font-inherit">
@@ -44,24 +44,23 @@ const PortfolioTemplate = () => {
               clean code to bring ideas to life.
             </p>
           </div>
-          <button
-            className="cursor-pointer [border:none] py-[1.5rem] px-[4rem] bg-midnightblue-500 rounded-9xl w-[12.88rem] flex flex-row items-center justify-center box-border"
-            onClick={onButtonClick}
-          >
+          <Link href="#contact">
+          <button className="cursor-pointer [border:none] py-[1.5rem] px-[4rem] bg-midnightblue-500 rounded-9xl w-[12.88rem] flex flex-row items-center justify-center box-border">
             <div className="flex-1 relative text-[1.25rem] leading-[1.88rem] font-fredoka-one text-white text-center">
               Contact
             </div>
           </button>
+          </Link>
         </div>
       </section>
-      <section className="self-stretch bg-steelblue overflow-hidden flex flex-row items-center justify-center text-center text-[1.69rem] text-cornsilk-300 font-body-text">
-        <div className="flex-1 flex flex-row flex-wrap items-start justify-start gap-[1.5rem]">
+      <section id="about" className="self-stretch bg-deepskyblue-100 overflow-hidden flex flex-row items-center justify-center text-center text-[1.69rem] text-cornsilk-300 font-body-text my-1">
+        <div className="flex-1 flex flex-row flex-wrap items-center justify-center gap-[1.5rem]">
           <img
             className="relative w-[11.94rem] h-[31.5rem] object-cover sm:hidden"
             alt=""
             src="/slice-3-1@2x.png"
           />
-          <div className="flex-1 bg-midnightblue-400 flex flex-col items-center justify-center py-[3rem] px-[2.63rem] box-border gap-[2.63rem] min-w-[20rem]">
+          <div className="flex-1 bg-midnightblue-400 flex flex-col items-center justify-center sm:mt-[1rem] md:mt-[3rem] py-[3rem] px-[2.63rem] box-border gap-[2.63rem] min-w-[18rem]">
             <img
               className="relative w-[7.96rem] h-[7.75rem]"
               alt=""
@@ -97,135 +96,81 @@ const PortfolioTemplate = () => {
           />
         </div>
       </section>
-      <section className="self-stretch bg-deepskyblue-100 overflow-hidden flex flex-col items-start justify-start py-[2.63rem] px-[1.5rem] gap-[2.63rem] text-center text-[2rem] text-black font-body-text">
-        <h1 className="m-0 relative text-inherit leading-[2.63rem] font-semibold font-inherit">
+      <section className="self-stretch bg-deepskyblue-100 overflow-hidden flex flex-col items-center justify-start py-[2.63rem] px-[1.5rem] gap-[2.63rem] text-center text-[2rem] text-black font-fredoka-one mb-1">
+        <h1 className="m-0 relative text-inherit leading-[2.63rem] font-normal font-inherit">
           My Skills
         </h1>
-        <div className="self-stretch bg-cornsilk-600 h-[21.56rem] flex flex-row flex-wrap items-start justify-start py-[4rem] px-[2.63rem] box-border gap-[2.63rem] min-w-[20rem] text-left text-[1.69rem]">
-          <img
-            className="relative w-[4rem] h-[4rem] hidden"
-            alt=""
-            src="/skills-card-icon3.svg"
-          />
-          <div className="flex-1 hidden flex-col items-start justify-start gap-[0.75rem] min-w-[12.5rem]">
-            <div className="self-stretch relative leading-[2.63rem] font-semibold">
-              Title
+        <div className="shadow-[5px_5px_0px_0px_rgba(109,40,217)] bg-midnightblue-500 p-8 flex flex-wrap items-center justify-center box-border min-w-[18rem] ">
+        <div className="mt-16 flex flex-wrap gap-12 ">
+          {skills.map((skill) => (
+            <div className="block-container w-20 h-20" key={skill.name}>
+              <div className="btn-back rounded-xl btn-back-black" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <Image
+                  className={`w-1/2 h-1/2 object-contain`}
+                  height={`1/2`}
+                  width={`1/2`}
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                />
+              </div>
             </div>
-            <div className="self-stretch relative text-[1.06rem] leading-[1.69rem]">
-              This is a template Figma file, turned into code using Anima. Learn
-              more at AnimaApp.com
-            </div>
-          </div>
+          ))}
+        </div>
         </div>
       </section>
-      <section className="self-stretch bg-deepskyblue-200 flex flex-col items-center justify-start py-[2.63rem] px-[1.5rem] gap-[2.63rem] text-center text-[2.5rem] text-floralwhite font-fredoka-one">
-        <h1 className="m-0 relative text-inherit leading-[2.63rem] font-normal font-inherit">
+      <section id="projects" className="self-stretch bg-deepskyblue-200 flex flex-col items-center justify-start py-[2.63rem] px-[1.5rem] gap-[2.63rem] text-center text-[2.5rem] text-black font-fredoka-one">
+        <h1 className="m-0 relative text-inherit leading-[2.63rem] font-light font-inherit">
           Projects
         </h1>
-        <div className="self-stretch flex flex-row flex-wrap items-start justify-start gap-[1.5rem] text-left text-[1.25rem] text-black font-body-text">
-          <div className="flex-1 flex flex-col items-start justify-start gap-[1.5rem] min-w-[20rem]">
-            <img
-              className="self-stretch relative max-w-full overflow-hidden h-[20.63rem] shrink-0 object-cover"
-              alt=""
-              src="/image1@2x.png"
-            />
-            <div className="self-stretch flex flex-col items-start justify-start gap-[0.5rem]">
-              <div className="self-stretch relative leading-[1.88rem] font-semibold">
-                Project title
+        <div className="flex flex-wrap m-20 sm:m-4 gap-16">
+          {projects.map((project) => (
+            <div className="lg:w-[400px] w-full" key={project.name}>
+              <div className="block-container w-12 h-12 ml-4">
+                <div className={`btn-back rounded-xl ${project.theme}`} />
+                <div className="btn-front rounded-xl flex justify-center items-center">
+                  <Image
+                    src={project.iconUrl}
+                    alt="threads"
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
               </div>
-              <div className="self-stretch relative text-[1.06rem] leading-[1.69rem]">
-                UI, Art drection
-              </div>
-            </div>
-          </div>
-          <div className="flex-1 flex flex-col items-start justify-start gap-[1.5rem] min-w-[20rem]">
-            <img
-              className="self-stretch relative max-w-full overflow-hidden h-[20.63rem] shrink-0 object-cover"
-              alt=""
-              src="/image2@2x.png"
-            />
-            <div className="self-stretch flex flex-col items-start justify-start gap-[0.5rem]">
-              <div className="self-stretch relative leading-[1.88rem] font-semibold">
-                Project title
-              </div>
-              <div className="self-stretch relative text-[1.06rem] leading-[1.69rem]">
-                UI, Art drection
-              </div>
-            </div>
-          </div>
-          <div className="flex-1 flex flex-col items-start justify-start gap-[1.5rem] min-w-[20rem]">
-            <img
-              className="self-stretch relative max-w-full overflow-hidden h-[20.63rem] shrink-0 object-cover"
-              alt=""
-              src="/image3@2x.png"
-            />
-            <div className="self-stretch flex flex-col items-start justify-start gap-[0.5rem]">
-              <div className="self-stretch relative leading-[1.88rem] font-semibold">
-                Project title
-              </div>
-              <div className="self-stretch relative text-[1.06rem] leading-[1.69rem]">
-                UI, Art drection
+
+              <div className="mt-5 flex flex-col items-center text-mid">
+                <h4 className=" font-poppins text-xl font-medium  ">{project.name}</h4>
+                  <Image
+                    src={project.imageUrl}
+                    alt="threads"
+                    className="w-full h-1/2 sm:w-[18rem] object-contain"
+                  />
+                <p className="mt-5 text-slate-950">{project.description}</p>
+                <div className="mt-1 flex items-center gap-2 font-poppins">
+                  <Link href={project.link} className=" text-mid text-blue-600">
+                    Live Link
+                  </Link>
+                  <Image
+                    src={arrow}
+                    alt="arrow"
+                    className="w-4 h-4 object-contain"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex-1 flex flex-col items-start justify-start gap-[1.5rem] min-w-[20rem]">
-            <img
-              className="self-stretch relative max-w-full overflow-hidden h-[20.63rem] shrink-0 object-cover"
-              alt=""
-              src="/image4@2x.png"
-            />
-            <div className="self-stretch flex flex-col items-start justify-start gap-[0.5rem]">
-              <div className="self-stretch relative leading-[1.88rem] font-semibold">
-                Project title
-              </div>
-              <div className="self-stretch relative text-[1.06rem] leading-[1.69rem]">
-                UI, Art drection
-              </div>
-            </div>
-          </div>
-          <div className="flex-1 flex flex-col items-start justify-start gap-[1.5rem] min-w-[20rem]">
-            <img
-              className="self-stretch relative max-w-full overflow-hidden h-[20.63rem] shrink-0 object-cover"
-              alt=""
-              src="/image5@2x.png"
-            />
-            <div className="self-stretch flex flex-col items-start justify-start gap-[0.5rem]">
-              <div className="self-stretch relative leading-[1.88rem] font-semibold">
-                Project title
-              </div>
-              <div className="self-stretch relative text-[1.06rem] leading-[1.69rem]">
-                UI, Art drection
-              </div>
-            </div>
-          </div>
-          <div className="flex-1 flex flex-col items-start justify-start gap-[1.5rem] min-w-[20rem]">
-            <img
-              className="self-stretch relative max-w-full overflow-hidden h-[20.63rem] shrink-0 object-cover"
-              alt=""
-              src="/image6@2x.png"
-            />
-            <div className="self-stretch flex flex-col items-start justify-start gap-[0.5rem]">
-              <div className="self-stretch relative leading-[1.88rem] font-semibold">
-                Project title
-              </div>
-              <div className="self-stretch relative text-[1.06rem] leading-[1.69rem]">
-                UI, Art drection
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
-      <section className="self-stretch bg-midnightblue-200 flex flex-row items-start justify-start pt-[2.63rem] px-[1.5rem] pb-[0.06rem] gap-[1.5rem] text-left text-[2rem] text-cornsilk-200 font-body-text">
-        <div className="flex-1 flex flex-row flex-wrap items-start justify-start gap-[8.88rem]">
-          <h1 className="m-0 relative text-inherit leading-[2.63rem] font-semibold font-inherit inline-block w-[39.13rem] shrink-0">
+      <section id="contact" className="self-stretch bg-midnightblue-200 flex flex-row items-start justify-start pt-[2.63rem] pl-[1.5rem] pb-[0.06rem] gap-[1.5rem] text-left text-[2rem] text-cornsilk-200 font-body-text">
+        <div className="flex-1 flex flex-row flex-wrap items-start justify-start gap-[1.88rem]">
+          <h1 className="m-0 relative text-inherit leading-[2.63rem] font-semibold font-inherit inline-block w-[39.13rem] sm:w-auto   shrink-0">
             Contact
           </h1>
-          <div className="flex-1 flex flex-col items-start justify-start min-w-[20rem] text-[1.06rem] text-cornsilk-400">
+          <div className="flex-1 flex flex-col items-start justify-start min-w-[18rem] mb-4 text-[1.06rem] text-cornsilk-400">
             <div className="self-stretch flex flex-col items-start justify-start gap-[2.63rem]">
               <p className="m-0 self-stretch relative leading-[1.69rem]">
-                This is a template Figma file, turned into code using Anima.
-                Learn more at AnimaApp.com This is a template Figma file, turned
-                into code using Anima. Learn more at AnimaApp.com
+                If you have a project you'd like to discuss or are interested in
+                bringing my skills and experience onto your software engineering
+                team, please don't hesitate to get in touch
               </p>
               <p className="m-0 self-stretch relative leading-[1.69rem]">
                 aspenndungu@gmail.com
@@ -234,7 +179,7 @@ const PortfolioTemplate = () => {
           </div>
         </div>
         <img
-          className="flex-1 relative max-w-full overflow-hidden h-[16.81rem] object-cover"
+          className="flex-1 relative max-w-full overflow-hidden h-[16.81rem] object-cover rounded-tl-3xl"
           alt=""
           src="/slice-4-11@2x.png"
         />
