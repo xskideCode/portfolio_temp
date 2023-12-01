9e497052e297742eafb8051dc3cfe85f52fdcf69
xskide <xskide.code@gmail.com>
2023-12-01 13:18:07 +0300

    2.1 commit

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,15 +1,15 @@
 const page = () => {
   return (
     <div className="relative bg-midnightblue-300 h-fit w-full flex flex-col items-center justify-start">
-      <div class="flex min-h-[80vh] items-start justify-center px-40 md:px-10">
+      <div class="flex min-h-[80vh] items-start justify-center px-40 md:px-10 bg-cornsilk-200">
         <div class="grid grid-cols-12 ">
-          <div class="col-span-12 lg:col-start-4 lg:col-span-6">
-            <h5 class="block antialiased tracking-normal font-sans text-xl leading-snug text-inherit mb-1 font-bold !text-cornsilk-100">
-              {" "}
-              Hi! 👋 My name is Leon and I am a software engineer.?{" "}
+          <div class="col-span-12 lg:col-start-4 lg:col-span-6 font-body-text">
+            <h5 class="block antialiased tracking-normal font-inherit text-xl leading-snug text-inherit mb-1 font-semibold !text-black">
+              
+              Hi! 👋 My name is Leon and I am a software engineer.
             </h5>
-            <div class="block antialiased font-sans text-base leading-relaxed mb-4 font-normal text-cornsilk-700">
-              {" "}
+            <div class="block antialiased font-inherit text-base leading-relaxed mb-4 font-normal text-black">
+              
               I am a web developer with more than a year of experience in
               creating responsive web applications from scratch. I use the
               latest technologies and frameworks. I am also passionate about
@@ -17,25 +17,24 @@ const page = () => {
               people’s lives. I am always eager to learn new skills and explore
               new challenges in the field of AI.
             </div>
-            <hr class="my-6 border-blue-cornsilk-50" />
-            <h5 class="block antialiased tracking-normal font-sans text-xl leading-snug text-inherit mt-6 mb-1 font-bold !text-cornsilk-200">
-              {" "}
-              EDUCATION{" "}
+            <hr class="my-6 border-blue-black " />
+            <h5 class="block antialiased tracking-normal font-inherit text-xl leading-snug text-inherit mt-6 mb-1 font-semibold !text-black">
+              
+              EDUCATION
             </h5>
-            <div class="block antialiased font-sans text-base leading-relaxed mb-4 font-normal text-cornsilk-700">
-              {" "}
+            <div class="block antialiased font-inherit text-base leading-relaxed mb-4 font-normal text-black">
+              
               Bachelor of Science in Information Technology Jomo Kenyatta
-              University of Agriculture and Technology Relevant coursework: Web
-              Development, Database Systems, Software Engineering.
+              University of Agriculture and Technology.
             </div>
-            <hr class="my-6 border-blue-cornsilk-50" />
-            <hr class="my-6 border-blue-cornsilk-50" />
-            <h5 class="block antialiased tracking-normal font-sans text-xl leading-snug text-inherit mt-6 mb-1 font-bold !text-cornsilk-200">
-              {" "}
-              TECHNICAL SKILLS{" "}
+            <hr class="my-6 border-blue-black " />
+            <hr class="my-6 border-blue-black " />
+            <h5 class="block antialiased tracking-normal font-inherit text-xl leading-snug text-inherit mt-6 mb-1 font-semibold !text-black">
+              
+              TECHNICAL SKILLS
             </h5>
-            <div class="block antialiased font-sans text-base leading-relaxed mb-4 font-normal text-cornsilk-700">
-              {" "}
+            <div class="block antialiased font-inherit text-base leading-relaxed mb-4 font-normal text-black">
+              
               Programming Languages: JavaScript, Python, SQL Web Development:
               React, Node.js Tools and Frameworks: Git, MongoDB
             </div>
